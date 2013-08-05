a3ac0fdc116d6cab9cd2f7eedae4dba026ade0b1
Mattias Rydengren <mattias.rydengren@stratsys.se>
2013-08-05 07:26:11 +0200

    Removed 'gpcb'; instead been replaced by 'gpush'.

--- functions/git.gpcb.bash
+++
@@ -1,9 +0,0 @@
-gpcb () {
-    if [[ $1 = "help" ]]; then
-        echo "Usage: gpcb"
-        echo "Pushes the current branch to origin."
-    else
-        local branch=$(__rebash_git_current_branch)
-        git push origin $branch
-    fi    
-}
