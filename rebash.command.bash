18b221cc9b169458bbef7910e3b3481e6d4688a4
Mattias Rydengren <mattias.rydengren@stratsys.se>
2014-03-03 10:44:16 +0100

    Add 'rebash return' and update 'rebash go'.

--- rebash.command.bash
+++ rebash.command.bash
@@ -3,13 +3,19 @@ rebash_help () {
     echo ""
     echo "  -h, --help              display this help text and exit"    
     echo "  go                      go to the rebash directory"
+    echo "  return                  return to the previous directory, if any"
     echo "  uninstall               uninstall rebash"
     echo "  update                  update rebash"
     echo ""
 }
 
 rebash_go () {
+    pushd . > /dev/null
     cd ~/.rebash
+}
+
+rebash_return () {
+    pushd &> /dev/null
 }
 
 rebash_uninstall () {
@@ -21,11 +27,10 @@ rebash_uninstall () {
 }
 
 rebash_update () {
-    pushd . > /dev/null
     rebash go
     git pull --ff-only origin master
     reload
-    popd > /dev/null    
+    rebash return
 }
 
 rebash () {
@@ -35,6 +40,9 @@ rebash () {
             ;;
         go)
             rebash_go 
+            ;;
+        return)
+            rebash_return
             ;;
         uninstall)
             rebash_uninstall
