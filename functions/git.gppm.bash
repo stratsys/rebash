da9ecbf3c950fa0b5479a01c5845b2f6bf331d03
Tobias Eriksson <tobias.eriksson@stratsys.se>
2015-06-03 22:34:07 +0200

    Uppdaterat rebash för 6.1

--- functions/git.gppm.bash
+++ functions/git.gppm.bash
@@ -1,10 +1,10 @@
 gppm () {
     local release_branches=(
-        '5.7/master' 
         '5.8/master' 
-        '5.9/master'         
+        '5.9/master' 
         '6.0/master' 
         'dev/test'
+        '6.1/master'         
         'dev/master' 
         'vnext/master'
     )
