c98f5d74a84086bd5a96114b1ee579656db9ffde
Tobias Eriksson <tobias.eriksson@stratsys.se>
2016-03-02 09:40:37 +0100

    autocrlf ska alltid vara true

--- config/git.config.bash
+++ config/git.config.bash
@@ -3,3 +3,4 @@ git config --global core.quotepath off
 git config --global push.default simple
 git config --global --unset i18n.commitencoding
 git config --global --unset i18n.logoutputencoding
+git config --global core.autocrlf true
