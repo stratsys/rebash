c9424c53bd556bfbb4a2e1c454571f6b76e59e9c
Tobias Eriksson <tobias.eriksson@stratsys.se>
2018-08-10 09:17:44 +0200

    Ändrat så att gup mergar in ändringarna istället för rebase

--- functions/git.gup.bash
+++ functions/git.gup.bash
@@ -8,9 +8,14 @@ gup () {
         if [ "$release_branch" == "$feature_branch" ]; then
             echo "You're on a release branch."
         else
+            __rebash_echo_trace "Checking out '$release_branch'."
             git checkout $release_branch || return # might fail if we have uncommitted changes.
+            __rebash_echo_trace "Pulling '$release_branch'."
             git pull --ff-only 
-            git rebase $release_branch $feature_branch
+            __rebash_echo_trace "Checking out '$feature_branch'."
+            git checkout $feature_branch
+            __rebash_echo_trace "Merging '$release_branch' into '$feature_branch'."
+            git merge $release_branch
         fi
     fi
 }
