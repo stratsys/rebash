33deb2503e01f2843ebea5b58e99d856713cf3d1
anton.sjoberg <anton.sjoberg@stratsys.se>
2020-06-11 10:07:10 +0200

    DEVOPS-1061: Automatisera processen kring att "låsa" dev/master till dev/test justeringsrelease

--- aliases/git.aliases.bash
+++ aliases/git.aliases.bash
@@ -40,6 +40,9 @@ alias gss='git status'
 
 alias gclean='git clean -fx -d'
 
+alias gdevtotest='gcodev && gcodevtest && git merge --no-ff dev/master && git push && gogo'
+alias gtestto73='gcodevtest && gco73 && git merge --no-ff dev/test && git push && gogo'
+
 alias gco72='git checkout 7.2/master && gpl'
 alias gco73='git checkout 7.3/master && gpl'
 alias gcodevtest='git checkout dev/test && gpl'
