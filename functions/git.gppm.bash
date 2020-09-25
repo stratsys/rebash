cdbcb2571b2122d85a0e6a5c80cfaa68f0f9de64
Tobias Eriksson <tobias.eriksson@stratsys.se>
2020-09-25 09:10:26 +0200

    dev/test bygger nu på 7.4

--- functions/git.gppm.bash
+++ functions/git.gppm.bash
@@ -2,9 +2,9 @@ gppm () {
     local release_branches=(        
         '7.2/master'
         'lts/master'        
-        '7.3/master'
-        'dev/test'
+        '7.3/master'        
         '7.4/master'
+        'dev/test'
         'dev/master'
         'vnext/master'
     )
