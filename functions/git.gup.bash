758be57d964c449c47a2a2dca13826e6e0be1cd6
Tobias Eriksson <tobias.eriksson@stratsys.se>
2025-12-15 14:41:52 +0100

    STR-24358 Ändra så att gup i rebash inte uppdaterar från master utan att checka ut den (#7)

--- functions/git.gup.bash
+++ functions/git.gup.bash
@@ -1,20 +1,16 @@
 gup () {
     if [[ $1 = help ]]; then
         echo "Usage: gup"
-        echo "Update feature branche with latest commits from release branch."
+        echo "Update feature branche with latest commits from origin master branch."
     else
         local release_branch=$(__rebash_git_relative_branch) feature_branch=$(__rebash_git_current_branch)
         
         if [ "$release_branch" == "$feature_branch" ]; then
-            echo "You're on a release branch."
+            echo "You're on a master branch."
         else
-            __rebash_echo_trace "Checking out '$release_branch'."
-            git checkout $release_branch || return # might fail if we have uncommitted changes.
-            __rebash_echo_trace "Pulling '$release_branch'."
-            git pull --ff-only 
-            __rebash_echo_trace "Checking out '$feature_branch'."
-            git checkout $feature_branch
-            __rebash_echo_trace "Merging '$release_branch' into '$feature_branch'."
+            __rebash_echo_trace "Fetching latest from origin '$release_branch'."
+            git fetch origin $release_branch
+            __rebash_echo_trace "Merging origin '$release_branch' into '$feature_branch'."
             git merge $release_branch
         fi
     fi
